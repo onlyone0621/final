6a241629d36dc5acf83452c79fa4fc261e2bc9e5
Kwon-SoonWook <tnsdnrzz@naver.com>
2025-06-17 10:19:09 +0900

    프로젝트 생성

---
+++ cbo/src/main/java/com/cbo/CboApplication.java
@@ -0,0 +1,13 @@
+package com.cbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CboApplication.class, args);
+	}
+
+}
---
+++ cbo/src/test/java/com/cbo/CboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
