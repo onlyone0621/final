0a1401f8f12128c59b28fd92b495d8e5143e005a
jdy7149 <184757480+jdy7149@users.noreply.github.com~>
2025-06-17 11:56:22 +0900

    Add ApprovalConst

---
+++ cbo/src/main/java/com/cbo/constant/ApprovalConst.java
@@ -0,0 +1,8 @@
+package com.cbo.constant;
+
+public class ApprovalConst {
+	public static final String APPROVED = "결재 완료";
+	public static final String PENDING = "결재 대기";
+	public static final String REJECTED = "반려";
+	public static final String REFERENCE = "참조";
+}
