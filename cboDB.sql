6cbf16317069a3d3c336dc98e7735775f3a42d56
jdy7149 <184757480+jdy7149@users.noreply.github.com~>
2025-06-18 17:30:51 +0900

    Fix errors in sql

--- cboDB.sql
+++ cboDB.sql
@@ -5,7 +5,7 @@ drop table community;
 drop table draft;
 drop table medical_support;
 drop table leave_application;
-drop table approvalline;
+drop table approval_line;
 drop table approval_doc;
 drop table chat_message;
 drop table chatroom_user;
@@ -13,8 +13,8 @@ drop table chatroom;
 drop table drive;
 drop table message;
 drop table calendar;
-drop table groups;
 drop table addr_group;
+drop table groups;
 drop table addr;
 drop table member;
 drop table dept;
@@ -29,6 +29,7 @@ drop sequence sq_drive_id;
 drop sequence sq_chatroom_id;
 drop sequence sq_chat_message_id;
 drop sequence sq_addr_id;
+drop sequence sq_groups_id;
 drop sequence sq_calendar_id;
 drop sequence sq_approval_doc_id;
 drop sequence sq_approval_line_id;
@@ -87,7 +88,7 @@ create table member(
     email varchar2(100) not null,
     address varchar2(100) not null,
     tel varchar2(100) not null,
-    join_date date not null DEFAULT SYSDATE,
+    join_date date DEFAULT SYSDATE not null,
     dept_id number(10),
     grade_id number(10),
     profile_image varchar2(300),
@@ -102,7 +103,7 @@ create table message(
     content varchar2(300),
     receiver_id number(10),
     sender_id number(10),
-    write_date date not null DEFAULT SYSDATE,
+    write_date date DEFAULT SYSDATE NOT NULL,
     is_read varchar2(60) default '안 읽음' not null,
     file_name varchar2(300) not null,
     ref number(10) not null,
@@ -117,7 +118,7 @@ create table drive(
     id number(10) primary key,
     member_id number(10),
     uploader varchar2(100) not null,
-    upload_date date not null DEFAULT SYSDATE,
+    upload_date date DEFAULT SYSDATE NOT NULL,
     path varchar2(500) not null,
     foreign key (member_id) references member(id) ON DELETE CASCADE 
 
@@ -128,7 +129,7 @@ create table chatroom(
     id number(10) primary key,
     name varchar2(100) not null,
     description varchar2(200) not null,
-    create_date date not null DEFAULT SYSDATE
+    create_date date DEFAULT SYSDATE NOT NULL
 
 );
 
@@ -137,7 +138,7 @@ create table chat_message(
     id number(10) primary key,
     chatroom_id number(10) not null,
     member_id number(10),
-    write_date date not null DEFAULT SYSDATE,
+    write_date date DEFAULT SYSDATE NOT NULL,
     content varchar2(1000) not null,
     foreign key (chatroom_id) references chatroom(id) ON DELETE CASCADE ,
     foreign key (member_id) references member(id) ON DELETE SET NULL  
@@ -147,7 +148,7 @@ create table chatroom_user(
 
     chatroom_id number(10) not null,
     member_id number(10) not null,
-    join_date date not null DEFAULT SYSDATE,
+    join_date date DEFAULT SYSDATE NOT NULL,
     foreign key (chatroom_id) references chatroom(id) ON DELETE CASCADE,
     foreign key (member_id) references member(id) ON DELETE CASCADE  
 
@@ -181,7 +182,7 @@ create table addr_group(
     addr_id number(10) not null,
     groups_id number(10) not null,
     foreign key (addr_id) references addr(id) ON DELETE CASCADE,
-    foreign key (addr_group_id) references addr(id) ON DELETE CASCADE
+    foreign key (groups_id) references groups(id) ON DELETE CASCADE
 
 );
 
@@ -194,7 +195,7 @@ create table calendar(
     content varchar2(300) not null,
     start_time date not null,
     end_time date not null,
-    create_date date not null DEFAULT SYSDATE,
+    create_date date DEFAULT SYSDATE NOT NULL,
     foreign key (member_id) references member(id) ON DELETE CASCADE,
     foreign key (dept_id) references dept(id) ON DELETE CASCADE
 
@@ -206,19 +207,19 @@ create table approval_doc(
     title varchar2(100) not null,
     member_id number(10),
     dept_id number(10),
-    write_date date not null DEFAULT SYSDATE,
+    write_date date DEFAULT SYSDATE NOT NULL,
     retention NUMBER(5) not null,
     foreign key (member_id) references member(id) ON DELETE SET NULL,
     foreign key (dept_id) references dept(id) ON DELETE SET NULL
     
 );
 
-create table approval_line( --n? status?
+create table approval_line( 
 
     approval_doc_id number(10) NOT NULL,
     member_id number(10),
     status varchar2(100) default '결재 예정',
-    process_date DEFAULT NULL,     
+    process_date DATE DEFAULT NULL,     
     foreign key (approval_doc_id) references approval_doc(id) ON DELETE CASCADE,
     foreign key (member_id) references member(id) ON DELETE SET NULL
 );
@@ -230,7 +231,7 @@ CREATE TABLE draft (
     type VARCHAR2(100) NOT NULL,
     content VARCHAR2(300) NOT NULL,
     foreign key (approval_doc_id) REFERENCES approval_doc(id) ON DELETE CASCADE
-)
+);
 
 CREATE TABLE medical_support (
     id NUMBER(10) PRIMARY KEY,
@@ -241,7 +242,7 @@ CREATE TABLE medical_support (
     requested NUMBER(10) NOT NULL,
     oop NUMBER(10) NOT NULL,
     foreign key (approval_doc_id) REFERENCES approval_doc(id) ON DELETE CASCADE
-)
+);
 
 CREATE TABLE leave_application (
     id NUMBER(10) PRIMARY KEY,
@@ -255,7 +256,7 @@ CREATE TABLE leave_application (
     remaining NUMBER(5) NOT NULL,
     reason VARCHAR2(300) NOT NULL,
     foreign key (approval_doc_id) REFERENCES approval_doc(id) ON DELETE CASCADE
-)
+);
 
 
 create table community(
@@ -263,7 +264,7 @@ create table community(
     id number(10) primary key,
     name varchar2(100) not null,
     description varchar2(100) not null,
-    create_date date not null DEFAULT SYSDATE,
+    create_date date DEFAULT SYSDATE NOT NULL,
     member_id number(10),
     foreign key (member_id) references member(id) ON DELETE SET NULL
 
@@ -276,7 +277,7 @@ create table community_post(
     title varchar2(100) not null,
     member_id number(10),
     content varchar2(300) not null,
-    write_date date not null DEFAULT SYSDATE,
+    write_date date DEFAULT SYSDATE NOT NULL,
     view_num number(10) default 0 not null,
     upvote number(10) default 0 not null,
     foreign key (community_id) references community(id) ON DELETE CASCADE,
@@ -289,7 +290,7 @@ create table community_member(
     community_id number(10) NOT NULL,
     member_id number(10) NOT NULL,
     type varchar2(100),
-    join_date date NOT NULL DEFAULT SYSDATE,
+    join_date date DEFAULT SYSDATE NOT NULL,
     foreign key (community_id) references community(id) ON DELETE CASCADE,
     foreign key (member_id) references member(id) ON DELETE CASCADE
 
@@ -301,11 +302,11 @@ create table community_comment(
     community_post_id number(10) not null,
     member_id number(10),
     content varchar2(300) not null,
-    write_date date not null DEFAULT SYSDATE,
+    write_date date DEFAULT SYSDATE NOT NULL,
     upvote number(10) default 0 not null,
     ref NUMBER(10) NOT NULL,
-    lev NUMBER(10) NOT NULL DEFAULT 0,
-    sunbun NUMBER(10) NOT NULL DEFAULT 0,
+    lev NUMBER(10) DEFAULT 0 NOT NULL,
+    sunbun NUMBER(10) DEFAULT 0 NOT NULL,
     foreign key (community_post_id) references community_post(id) ON DELETE CASCADE,
     foreign key (member_id) references member(id) ON DELETE SET NULL
 
