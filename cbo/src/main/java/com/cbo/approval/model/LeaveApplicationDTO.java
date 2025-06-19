80b731019b6544ffbb6396ae6246f6b03d685d2e
jdy7149 <184757480+jdy7149@users.noreply.github.com~>
2025-06-19 09:22:02 +0900

    Finish LeaveApplicationDTO

---
+++ cbo/src/main/java/com/cbo/approval/model/LeaveApplicationDTO.java
@@ -0,0 +1,25 @@
+package com.cbo.approval.model;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LeaveApplicationDTO {
+	private int id;
+	private int doc_id;
+	private String dept;
+	private String grade;
+	private String name;
+	private String type;
+	private Date startDate;
+	private Date endDate;
+	private int remaining;
+	private String reason;
+}
