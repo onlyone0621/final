bda8863e63727c7eda767dea16f3f158679b90a7
jdy7149 <184757480+jdy7149@users.noreply.github.com~>
2025-06-20 11:31:27 +0900

    Add doc list method to service and mapper

--- cbo/src/main/java/com/cbo/approval/service/ApprovalService.java
+++ cbo/src/main/java/com/cbo/approval/service/ApprovalService.java
@@ -2,10 +2,14 @@ package com.cbo.approval.service;
 
 import java.util.List;
 
+import com.cbo.approval.model.DocViewDTO;
 import com.cbo.approval.model.FormatDTO;
 import com.cbo.member.model.OrganDTO;
 
 public interface ApprovalService {
 	List<FormatDTO> getFormats() throws Exception;
 	List<OrganDTO> selectMembers() throws Exception;
+	List<DocViewDTO> getApprovalDocs(int id) throws Exception;
+	List<DocViewDTO> getReferenceDocs(int id) throws Exception;
+	List<DocViewDTO> getDraftDocs(int id) throws Exception;
 }
--- cbo/src/main/java/com/cbo/approval/service/ApprovalServiceImple.java
+++ cbo/src/main/java/com/cbo/approval/service/ApprovalServiceImple.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.cbo.approval.model.DocViewDTO;
 import com.cbo.approval.model.FormatDTO;
 import com.cbo.mapper.ApprovalMapper;
 import com.cbo.member.model.OrganDTO;
@@ -28,6 +29,27 @@ public class ApprovalServiceImple implements ApprovalService {
 	public List<OrganDTO> selectMembers() throws Exception {
 		// TODO Auto-generated method stub
 		return mapper.selectMembers();
+	}
+
+
+	@Override
+	public List<DocViewDTO> getApprovalDocs(int id) throws Exception {
+		// TODO Auto-generated method stub
+		return mapper.selectApprovalDocs(id);
+	}
+
+
+	@Override
+	public List<DocViewDTO> getReferenceDocs(int id) throws Exception {
+		// TODO Auto-generated method stub
+		return mapper.selectReferenceDocs(id);
+	}
+
+
+	@Override
+	public List<DocViewDTO> getDraftDocs(int id) throws Exception {
+		// TODO Auto-generated method stub
+		return mapper.selectDraftDocs(id);
 	}
 
 }
