f0ecaa0be762f083883807a6be02ba16b3b54562
joooyam <ohgnar058@naver.com>
2025-06-23 09:13:57 +0900

    community

---
+++ cbo/src/main/java/com/cbo/community/service/CommunityServiceImple.java
@@ -0,0 +1,20 @@
+package com.cbo.community.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cbo.community.model.CommunityDTO;
+import com.cbo.mapper.CommunityMapper;
+@Service
+public class CommunityServiceImple implements CommunityService {
+  @Autowired
+  private CommunityMapper mapper;
+	
+@Override
+public int insertCommunity(CommunityDTO dto) throws Exception {
+	
+	int result = mapper.insertCommunity(dto);
+	return result;
+}
+}
+
--- cbo/src/main/java/com/cbo/controller/CommunityController.java
+++ cbo/src/main/java/com/cbo/controller/CommunityController.java
@@ -1,10 +1,20 @@
 package com.cbo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cbo.community.model.CommunityDTO;
+import com.cbo.community.service.CommunityService;
 
 @Controller
 public class CommunityController {
+	
+	@Autowired
+	private CommunityService service;
+	
 	
 	public CommunityController() {
 		
@@ -24,13 +34,49 @@ public class CommunityController {
 		return "community/communityMainAll";
 	}
 	
-	@GetMapping("communityCreate")
-	public String mainCreate() {
-		return "community/communityCreate";
-	}
-	
 	@GetMapping("communityList")
 	public String CommunityList() {
 		return "community/communitylist";
+	}
+	//커뮤니티 생성
+	@GetMapping("communityCreate")
+	public String communityCreate() {
+		return "community/manage/communityCreate";
+	}
+	
+	@PostMapping("communityCreate")
+	public ModelAndView insertCommunity(CommunityDTO dto) {
+	    int result = 0;
+	    String msg = null;
+
+	    try {
+	        dto.setMember_id(1); //임시로 했음
+	        result = service.insertCommunity(dto);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+
+	    msg = result > 0 ? "커뮤니티 생성 성공!" : "커뮤니티 생성 실패!";
+	    ModelAndView mav = new ModelAndView();
+	    mav.addObject("msg", msg); 
+	    mav.setViewName("community/communityMsg"); 
+	    return mav;
+	}
+	
+	
+	// 커뮤니티 정보 수정
+	@GetMapping("communityUpdate")
+	public String communityUpdate() {
+		return "community/manage/communityUpdate";
+	}
+	//커뮤니티 멤버 관리
+	@GetMapping("communityMember")
+	public String communityMember() {
+		return "community/manage/communityMember";
+	}
+	//커뮤니티 페쇄
+	@GetMapping("communityClose")
+	public String communityClose() {
+		return "community/manage/communityClose";
 	}
 }
